9f846fe21684f21e946cb6ebef2a6e216f020214
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-01 18:26:35 -0300

    update room view

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("room/<str:pk>", views.room, name="room"),
-    path("create-room/", views.create_room, name="create-room")
+    path("create-room/", views.create_room, name="create-room"),
+    path("edit-room/<str:pk>", views.update_room, name="edit-room")
 ]
