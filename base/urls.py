b60c97257e1bdbbc1f7be13eee878c4418a3eec1
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-01 18:38:44 -0300

    delete room view, url and template

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("room/<str:pk>", views.room, name="room"),
     path("create-room/", views.create_room, name="create-room"),
-    path("edit-room/<str:pk>", views.update_room, name="edit-room")
+    path("edit-room/<str:pk>", views.update_room, name="edit-room"),
+    path("delete-room/<str:pk>", views.delete_room, name="delete-room"),
 ]
