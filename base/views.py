c6c9c07280a9a51e5099a4ca82f8de880c657d9c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-02 11:02:38 -0300

    search and filter functionality

--- base/views.py
+++ base/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, "base/home.html", {"rooms": rooms})
+    q = request.GET.get('q')
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) | Q(description__icontains=q)) if q else Room.objects.all()
+
+    topics = Topic.objects.all()
+    rooms_count = rooms.count()
+    return render(request, "base/home.html", {"rooms": rooms, "topics": topics, "rooms_count": rooms_count})
 
 
 def room(request, pk):
