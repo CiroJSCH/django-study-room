0ae35c2105b4643006cd115c9f50a9f165534b2f
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-04 21:12:59 -0300

    user registration

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -12,11 +13,12 @@ from .forms import RoomForm
 
 
 def login_page(request):
+    page = "login"
     if request.user.is_authenticated:
         return redirect("home")
 
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -32,13 +34,30 @@ def login_page(request):
         else:
             messages.error(request, "Username OR password is incorrect")
 
-    context = {}
+    context = {"page": page}
     return render(request, "base/login_register.html", context)
 
 
 def logout_user(request):
     logout(request)
     return redirect("login")
+
+
+def register_user(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(
+                request, "An error has occurred during registration")
+    return render(request, "base/login_register.html", {"form": form})
 
 
 def home(request):
