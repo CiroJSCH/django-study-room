9b79c6f1e47ba230b4e4eb728e86dd6dc5446a1c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-18 18:54:49 -0300

    topics page view

--- base/views.py
+++ base/views.py
@@ -187,3 +187,9 @@ def update_user(request):
             form.save()
             return redirect("user-profile", pk=user.id)
     return render(request, "base/update_user.html", {"form": form})
+
+
+def topics_page(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, "base/topics.html", {"topics": topics})
